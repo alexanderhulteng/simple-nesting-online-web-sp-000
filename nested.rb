e1b30973315e336f7593112ede7f5db79350a58b
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-04-20 19:24:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -42,6 +42,7 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
+ big_name == ""
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -58,7 +59,8 @@ def dennis_ritchies_language
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages]
+		big_name == programmer_hash[:dennis_ritchie][:languages]
+		big_name
 end
 
 def adding_matz
